7a6796da58184c66629f7af69741e431a4d4cc21
ionescubogdan10 <ionescu.bogdan.ioan@cn-caragiale.ro>
2026-09-17 15:07:24 +0300

    am adaugat spatiul de joc al robotului si o miscare autonoma care face robotul sa ricoseze din pereti in timp ce merge pe diagonala

--- robo.py
+++ robo.py
@@ -3,8 +3,23 @@ root = tk.Tk()
 root.title("proiect robot")
 canvas = tk.Canvas(root, width=400, height=400, bg="white")
 canvas.pack()
-x=100;
-y=100;
-dimensiune = 150;
-robot = canvas.create_rectangle(x, y, x+dimensiune, y+dimensiune, fill="white", outline="black", width=2)
+x=45
+y=45
+dimensiune = 45
+viteza_x=2
+viteza_y=2
+canvas.create_rectangle(20, 20, 380, 380, fill="white", outline="black", width=2)
+def miscare_robot():
+    global x, y,viteza_x, viteza_y
+    x += viteza_x
+    y += viteza_y
+    if x < 20 or x + dimensiune > 380:
+        viteza_x = -viteza_x
+    if y < 20 or y + dimensiune > 380:
+        viteza_y = -viteza_y
+    canvas.coords(robot, x, y, x+dimensiune, y+dimensiune)
+    root.after(50, miscare_robot)
+robot = canvas.create_rectangle(x, y, x+dimensiune, y+dimensiune, fill="blue", outline="black", width=2)
+miscare_robot()
+    
 root.mainloop()
