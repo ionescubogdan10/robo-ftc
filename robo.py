e97f28664236ef161e8ccca0a3efd4c92d2d4073
ionescubogdan10 <ionescu.bogdan.ioan@cn-caragiale.ro>
2026-09-17 14:40:10 +0300

    am adaugat starea initiala a robotului si o forma geometrica pe care pot sa o mut cand si cum vreau eu

--- robo.py
+++ robo.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 root = tk.Tk()
-root.title("Tkinter Square Example")
+root.title("proiect robot")
 canvas = tk.Canvas(root, width=400, height=400, bg="white")
 canvas.pack()
 x=100;
 y=100;
 dimensiune = 150;
-robot = canvas.create_rectangle(x, y, x+dimensiune, y+dimensiune, fill="blue", outline="black", width=2)
+robot = canvas.create_rectangle(x, y, x+dimensiune, y+dimensiune, fill="white", outline="black", width=2)
 root.mainloop()
